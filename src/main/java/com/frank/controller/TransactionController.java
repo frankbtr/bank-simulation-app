c71c0d52990853f727fa1a7dc9e9c372be175e25
Frank Batur <frank_batur@outlook.com>
2023-04-03 21:03:18 -0400

    makeTransfer logic is done

--- src/main/java/com/frank/controller/TransactionController.java
+++ src/main/java/com/frank/controller/TransactionController.java
@@ -1,11 +1,16 @@
 package com.frank.controller;
 
+import com.frank.model.Account;
 import com.frank.model.Transaction;
 import com.frank.service.AccountService;
 import com.frank.service.TransactionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Date;
 
 @Controller
 public class TransactionController {
@@ -18,18 +23,32 @@ public class TransactionController {
         this.transactionService = transactionService;
     }
 
-    @GetMapping("make-transfer")
+    @GetMapping("/make-transfer")
     public String makeTransfer(Model model){
 
         //what we need to provide to make transfer happen
         //we need to provide empty transfer object
         model.addAttribute("transaction", Transaction.builder().build());
         //we need all accounts to provide them as sender, receiver
-        model.addAttribute("account", accountService.listAllAccount());
+        model.addAttribute("accounts", accountService.listAllAccount());
         //we need list of transaction(at least 10) to fill table,(business logic is missing)
-        model.addAttribute("lastTransaction", transactionService.last10Transactions());
-
+        model.addAttribute("lastTransactions", transactionService.last10Transactions());
 
         return "transaction/make-transfer";
+    }
+
+    //Task
+    //Write a post method, that takes transaction object from the method above
+    //complete the make transfer and return the message
+    @PostMapping("/transfer")
+    public String postMakeTransfer(@ModelAttribute("transaction") Transaction transaction){
+
+        // I have UUID and accounts, but I need to provide Account object!
+        // I need to find Accounts based on the ID that I have and use a parameters to makeTransfer
+        Account sender = accountService.retrieveById(transaction.getSender());
+        Account receiver = accountService.retrieveById(transaction.getReceiver());
+        transactionService.makeTransfer(sender, receiver, transaction.getAmount(), new Date(), transaction.getMessage());
+
+        return "redirect:/make-transfer";
     }
 }
